cbe75dcc5fb459798c5abae586db0ed5599ee38a
Chris Kankiewicz <Chris@ChrisKankiewicz.com>
2017-05-25 19:07:49 -0700

    Import classes in test case

--- tests/StrGenTest.php
+++ tests/StrGenTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use StrGen\Generator;
+
 class StrGenTest extends PHPUnit_Framework_TestCase
 {
     /** @var Generator Instance of StrGen\Generator */
@@ -7,55 +9,55 @@ class StrGenTest extends PHPUnit_Framework_TestCase
 
     public function test_it_can_be_initialized_without_arguments()
     {
-        $generator = new StrGen\Generator();
+        $generator = new Generator();
         $this->assertRegExp('/^.{16}$/', $generator->generate(16));
     }
 
     public function test_it_can_be_initialized_with_a_custom_charachter_set()
     {
-        $generator = new StrGen\Generator('0123456789abcdef');
+        $generator = new Generator('0123456789abcdef');
         $this->assertRegExp('/^[0-9a-f]{16}$/', $generator->generate(16));
     }
 
     public function test_it_can_be_initialized_with_the_lower_alpha_charachter_set()
     {
-        $generator = new StrGen\Generator(['lower']);
+        $generator = new Generator(['lower']);
         $this->assertRegExp('/^[a-z]{16}$/', $generator->generate(16));
     }
 
     public function test_it_can_be_initialized_with_the_upper_alpha_charachter_set()
     {
-        $generator = new StrGen\Generator(['upper']);
+        $generator = new Generator(['upper']);
         $this->assertRegExp('/^[A-Z]{16}$/', $generator->generate(16));
     }
 
     public function test_it_can_be_initialized_with_the_numeric_charachter_set()
     {
-        $generator = new StrGen\Generator(['numeric']);
+        $generator = new Generator(['numeric']);
         $this->assertRegExp('/^[0-9]{16}$/', $generator->generate(16));
     }
 
     public function test_it_can_be_initialized_with_the_special_charachter_set()
     {
-        $generator = new StrGen\Generator(['special']);
+        $generator = new Generator(['special']);
         $this->assertRegExp('/^[!@#$%^&*()-_=+.?]{16}$/', $generator->generate(16));
     }
 
     public function test_it_can_be_initialized_with_the_extra_charachter_set()
     {
-        $generator = new StrGen\Generator(['extra']);
+        $generator = new Generator(['extra']);
         $this->assertRegExp('/^[{}\[\]<>:;\/\\\|~]{16}$/', $generator->generate(16));
     }
 
     public function test_it_can_be_initialized_with_combined_charachter_sets()
     {
-        $generator = new StrGen\Generator(['upper', 'lower', 'numeric']);
+        $generator = new Generator(['upper', 'lower', 'numeric']);
         $this->assertRegExp('/^[a-zA-Z0-9]{16}$/', $generator->generate(16));
     }
 
     public function test_it_doesnt_generate_the_same_string_twice()
     {
-        $generator = new StrGen\Generator();
+        $generator = new Generator();
         $this->assertNotEquals($generator->generate(32), $generator->generate(32));
     }
 }
